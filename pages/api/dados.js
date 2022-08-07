86b8ed611041619505fd2754d64d36afb9a26416
Danilo da Silva Gomes <rk027133@brasil.ind.local>
2022-08-06 23:05:19 -0300

    atualização das páginas padrões

---
+++ pages/api/dados.js
@@ -0,0 +1,22 @@
+function dados(request, response) {
+    const dynamicDate = new Date();
+
+    response.json({
+        date: dynamicDate.toGMTString(),
+        Dolar: "R$ 5,16",
+        CDI: "13,15% a.a",
+        IPCA: "0,67% ultimo mês",
+        IPCA: "11,89% acumulado 12 meses",
+        INPC: "0,62% ultimo mês",
+        IGPM: "1,4%",
+        PIB: "R$ 8,7 tri ano",
+        PIB: "R$ 2,2 tri Trimestre",
+        PIB: "R$ 35.161,70 per Capita 2019",
+        TR: "0,2409",
+        IBOVESPA: "106.471.92",
+        Bitcoin: "119.464.38",
+        SalarioMinimo: "R$ 1.212",
+    });
+}
+
+export default dados
---
+++ pages/home.js
@@ -0,0 +1,38 @@
+import React from 'react'
+
+const home = () => {
+  return (
+    <div>
+        <nav>
+            <div>
+                <a>Home</a>
+                <a>About</a>
+                <a>Donation</a>
+            </div>
+        </nav>
+        <div>
+            <h1>Indicadores Economicos do Brasil</h1>
+            <h4>Principais indicadores brasileiros atualizados SEMPRE!</h4>
+        </div>
+        <div>
+            <ul>
+                <li>Dolar x Real | R$ 5,16</li>
+                <li>CDI | 13,15% a.a</li>
+                <li>IPCA | 0,67% ultimo mês</li>
+                <li>IPCA | 11,89% acumulado 12 meses</li>
+                <li>INPC | 0,62% ultimo mês</li>
+                <li>IGPM | 1,4%</li>
+                <li>PIB | R$ 8,7 tri ano</li>
+                <li>PIB | R$ 2,2 tri Trimestre</li>
+                <li>PIB | R$ 35.161,70 per Capita 2019</li>
+                <li>TR | 0,2409</li>
+                <li>IBOVESPA | 106.471.92</li>
+                <li>Bitcoin x Real | 119.464.38</li>
+                <li>Salário Minimo | R$ 1.212</li>
+            </ul>
+        </div>
+    </div>
+  )
+}
+
+export default home
