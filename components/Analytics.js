a6bd148b3ac0572623e66321a9e841be5438542e
Danilo da Silva Gomes <rk027133@brasil.ind.local>
2022-10-01 13:29:16 -0300

    inclusão id nos scripts do Google Analytics

--- components/Analytics.js
+++ components/Analytics.js
@@ -5,10 +5,12 @@ const gaId = process.env.GA_MEASUREMENT_ID;
 const Analytics = () => (
     <>
         <Script
+            id="google-analytics-path"
             strategy='lazyOnload'
             src='https://www.googletagmanager.com/gtag/js?id=${gaId}'
         />
         <Script
+            id="google-analytics-config"
             strategy='lazyOnload'
             dangerouslySetInnerHTML={{
                 __html: `
